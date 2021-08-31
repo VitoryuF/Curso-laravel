254b5a2649ef06654de7339a78d391631898ec5b
Vitor Ferreira Vaz <vitorvazemp@gmail.com>
2021-08-31 00:39:31 -0300

    Update AlunoController.php

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -4,34 +4,87 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AlunoController extends Controller
+class Alunocontroller extends Controller
 {
-    public function index(){
-        $alunos = ['Aluno 1', 'Aluno 2', 'Alunos 2'];
-        return $alunos;
+    protected $request;
+
+    public function __construct(Request $request){
+        dd($request -> show);
+        $this->request = $request;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
     }
 
-    public function del($id) {
-        return "Deletando cadastro do aluno $id";
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
-    public function update($id) {
-        return "Atualizando cadastro do aluno $id";
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
     }
 
-    public function create($id) {
-        return "Criando cadastro do aluno $id";
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return "Aluno $id °.";
     }
 
-    public function show($id){
-        return "Aluno selecionado $id";
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return "Editando o aluno $id.";
     }
 
-    public function cadastro() {
-        return "Cadastro de aluno!";
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
     }
 
-    public function edit($id) {
-        return "Editar cadastro do aluno $id";
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
