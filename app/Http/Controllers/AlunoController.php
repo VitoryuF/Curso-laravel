1be51e649ac8415bb3022b6f3ade10672987ebf8
Vitor Ferreira Vaz <vitorvazemp@gmail.com>
2021-08-31 23:57:00 -0300

    Update AlunoController.php

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -23,7 +23,7 @@ class Alunocontroller extends Controller
      */
     public function index()
     {
-
+        return view('welcome');
     }
 
     /**
@@ -33,7 +33,7 @@ class Alunocontroller extends Controller
      */
     public function create()
     {
-        return view('welcome');
+        return "Vitor";
     }
 
     /**
